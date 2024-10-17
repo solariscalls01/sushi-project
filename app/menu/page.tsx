3d22b3eb853303cb41b3cbfc4c10bbd42b07a427
Brian Dy <dyb@oregonstate.edu>
2024-10-16 20:01:45 -0700

    added menu links and icons.

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import Header from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import { createTheme, Typography } from "@mui/material";
+import NavLinksMenu from "./dashboard/nav-links";
 export default function Page() {
+  const theme = createTheme();
+  theme.spacing(24);
   return (
+    
     <div>
       <Header />
+      <Typography variant="h2" sx={{justifyContent: 'center', textAlign: 'center', mt: 3, mb: 3}}> Menu</Typography>
+      <NavLinksMenu ></NavLinksMenu>
       <Footer />
     </div>
   );

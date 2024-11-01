cc8acb3d4c9bacc9b60450bdd15b688f9e0caa41
Brian Dy <dyb@oregonstate.edu>
2024-11-01 13:07:32 -0700

    merge branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Header from "../components/NavBar";
 import Footer from "../components/Footer";
-import BackgroundImg from "../components/backgroundImg";
 import HomepageMap from "../components/homepage-map-hours";
 import MenuImageList from "../components/homepage-images";
 import CarouselImage from "../components/CarouselHome";
@@ -10,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <BackgroundVideo />
+      <CarouselImage />
       <HomepageMap />
       <MenuImageList />
       <Footer />
